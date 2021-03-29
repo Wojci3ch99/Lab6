6a0a9e595d5f5828e3c8c50b1067032b5a09d027
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-29 20:33:17 +0200

    Zadanie 9

--- src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
+++ src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +14,9 @@ public class TestNioCopy {
         Path oryginal = Paths.get("sample_1920×1280.tiff");
         Path kopia = Paths.get("img_copy2.tiff");
         Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+//        File oryginal = new File("sample_1920×1280.tiff");
+//        File kopia = new File("img_copy2.tiff");
+//        Files.copy(oryginal.toPath(), kopia.toPath());
 //        System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
         System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
     }
