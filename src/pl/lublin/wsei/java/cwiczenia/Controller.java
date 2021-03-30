fcbc76205b4c3e356ed603730b7048b2927aa540
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-30 13:21:45 +0200

    Zadanie 15 - wybór pliku .csv

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,4 +1,27 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+    public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("Pliki CSV (*.csv)", "*.csv");
+
+    public void initialize() {
+        fileChooser.getExtensionFilters().add(csvFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if (file !=null) {
+            lbFile.setText(file.getAbsolutePath());
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik ...");
+        }
+    }
 }
--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Noblisci.fxml"));
+        primaryStage.setTitle("Nobliści");
+        primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
     }
 
